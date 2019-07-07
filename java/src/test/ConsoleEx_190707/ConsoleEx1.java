4744a4994d8ab69177e601ea2045de87d66b0707
chayoung.choi <chayoung.dev@gmail.com>
2019-07-07 23:19:34 +0900

    QuizEx3 / ConsoleEx1,2
    QuizEx3 / ConsoleEx1,2

---
+++ java/src/test/ConsoleEx_190707/ConsoleEx1.java
@@ -0,0 +1,69 @@
+package test.ConsoleEx_190707;
+
+import java.util.Scanner;
+
+/*	190707.
+ *  이번 문제는 도스창과 같은 콘솔어플리케이션을 만들어보는 것입니다. 도스창 처럼 명령어를 입력하면
+	명령어가 실행된 결과를 화면에 보여주는 텍스트 기반의 어플리케이션입니다.(text-based application)
+
+	[문제1] 에제를 실행하면 '>>'와 같은 프롬프트가 나타나서 사용자의 입력을 기다리고,
+	        사용자가 입력한 내용을 화면에 출력한다. 만일 사용자가 'q' 또는 'Q'를 입력하면 프로그램을 종료한다.
+	        다음의 예제를 완성하세요.
+	[실행결과]
+	
+	>>hello
+	hello
+	>>help
+	help
+	>>ok
+	ok
+	>>q
+
+	[출처] [Java1000제] 콘솔 만들기 1 - 도스창 따라하기 (남궁성의 코드초보스터디(자바 java, c언어, javascript, python) |작성자 남궁성
+ */
+public class ConsoleEx1 {
+	public static void main(String[] args) {
+		while (true) {
+			String prompt = ">>";
+			System.out.print(prompt);
+			/*
+			 * 다음과 같은 내용의 코드를 넣으세요.
+			 * 1. 화면으로부터 라인단위로 입력받는다. 
+			 * - java.util.Scanner클래스 사용
+			 * 2. q 또는 Q를 입력하면 실행종료한다.
+			 */
+			Scanner s = new Scanner(System.in);
+			String input = s.nextLine();
+			
+			if ( "Q".equalsIgnoreCase(input) ) {
+				break;
+			}
+			
+			System.out.println(input);
+
+		} // while(true)
+	} // main
+}
+/*
+[원문]
+class ConsoleEx1 { 
+      public static void main(String[] args) { 
+            while(true) { 
+                  String prompt = ">>"; 
+                  System.out.print(prompt); 
+                  
+
+                  
+
+                     다음과 같은 내용의 코드를 넣으세요.                  
+
+                     1. 화면으로부터 라인단위로 입력받는다.  - java.util.Scanner클래스 사용
+                     2. q 또는 Q를 입력하면 실행종료한다.
+
+                  
+
+
+             } // while(true) 
+      } // main 
+} // class
+*/
---
+++ java/src/test/SimpleQuiz_190706/QuizEx3.java
@@ -0,0 +1,148 @@
+package test.SimpleQuiz_190706;
+
+import java.util.Scanner;
+
+/* 190707
+	[문제3] 문제2에서 완성된 코드를 이용해서, 문제의 순서를 임의로 바꾸고, 선택지도 임의로 바뀌어 출력되도록 하세요.
+	
+	void shuffle(String[] data) - 주어진 문자열배열의 각 요소의 순서를 임으로 뒤바꾼다.
+	
+	[실행결과]
+	[1] 다음 중 자바의 연산자가 아닌 것은?
+	1./     2.|     3.++    4.!=    5.^     6.&
+	[답]5
+	
+	[2] 다음 중 키워드가 아닌 것은?
+	1.final 2.True  3.if    4.public
+	[답]2
+	
+	[3] 다음 중 메서드의 반환값이 없음을 의미하는 키워드는?
+	1.null  2.void  3.false
+	[답]2
+	
+	정답개수/전체문항수 :3/3
+
+	[출처] [Java1000제] Simple Quiz 3 - 간단한 다지선다형 문제 (남궁성의 코드초보스터디(자바 java, c언어, javascript, python) |작성자 남궁성
+*/
+public class QuizEx3 {
+	public static void main(String[] args) {
+		String[] data = { "다음 중 키워드가 아닌 것은?`2`final`True`if`public",
+				"다음 중 자바의 연산자가 아닌 것은?`6`&`|`++`!=`/`^",
+				"다음 중 메서드의 반환값이 없음을 의미하는 키워드는?`1`void`null`false`", };
+
+		Scanner s = new Scanner(System.in);
+		int score = 0;
+
+		shuffle(data); // 문제를 섞는다.
+
+		for (int i = 0; i < data.length; i++) {
+			String[] tmp = data[i].split("`", 3);
+
+			String question = tmp[0];
+			String answer = tmp[1];
+			String[] choices = tmp[2].split("`");
+
+			answer = choices[Integer.parseInt(answer) - 1];
+
+			shuffle(choices); // 선택지를 섞는다.
+
+			System.out.println("[" + (i + 1) + "] " + question);
+
+			for (int x = 0; x < choices.length; x++) {
+				System.out.print(( x + 1 ) + "." + choices[x] + "\t");
+			}
+
+			System.out.println();
+			System.out.print("[답]");
+			String input = s.nextLine();
+
+			if (input.equals(answer)) {
+				score++;
+			}
+
+			System.out.println();
+			System.out.println();
+		}
+
+		System.out.println("정답개수/전체문항수 :" + score + "/" + data.length);
+	} // main
+
+	public static void shuffle(String[] data) {
+
+		// 코드를 완성하세요.
+
+		// 1. 배열data의 개수가 0보다 같거나 적으면 메서드 전체를 빠져나간다.
+		// 2. 선택지의 순서를 뒤바꾼다. 반복문과 Math.random()사용
+		if ( data.length <= 0 ) {
+			return;
+		}
+		
+		for (int i=0; i<data.length; i++) {
+			int idx = (int) (Math.random() * data.length);
+			String tmp = data[i];
+			data[i] = data[idx];
+			data[idx] = tmp;
+			
+		}
+	} // shuffle()
+}
+/*
+ * [원문]
+class QuizEx3 { 
+      public static void main(String[] args) { 
+            String[] data = { 
+                  "다음 중 키워드가 아닌 것은?`2`final`True`if`public", 
+                  "다음 중 자바의 연산자가 아닌 것은?`6`&`|`++`!=`/`^", 
+                  "다음 중 메서드의 반환값이 없음을 의미하는 키워드는?`1`void`null`false`", 
+            }; 
+            
+            Scanner s = new Scanner(System.in); 
+            int score = 0; 
+
+            shuffle(data); // 문제를 섞는다.
+
+            for(int i=0;i < data.length;i++) { 
+                  String[] tmp = data[i].split("`",3); 
+
+                  String question = tmp[0]; 
+                  String answer = tmp[1]; 
+                  String[] choices = tmp[2].split("`"); 
+
+                  answer = choices[Integer.parseInt(answer)-1]; 
+
+                  shuffle(choices); // 선택지를 섞는다.
+
+                  System.out.println("["+(i+1)+"] "+question); 
+
+                  for(int x=0;x < choices.length;x++) { 
+
+                               //코드를 완성하세요.
+
+
+                  } 
+
+                  System.out.println(); 
+                  System.out.print("[답]"); 
+                  String input = s.nextLine(); 
+                  
+                  if(input.equals(answer)) { 
+                        score++; 
+                  } 
+
+                  System.out.println(); 
+                  System.out.println(); 
+            } 
+
+            System.out.println("정답개수/전체문항수 :"+score+"/"+data.length); 
+      } // main 
+
+      public static void shuffle(String[] data) {
+
+           // 코드를 완성하세요.
+
+           //  1. 배열data의 개수가 0보다 같거나 적으면 메서드 전체를 빠져나간다.
+
+           //  2. 선택지의 순서를 뒤바꾼다. 반복문과 Math.random()사용
+      } // shuffle() 
+}
+ */
