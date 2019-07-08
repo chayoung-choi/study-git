c298e5b2d1464feb81410d40451b77f2511dba26
chayoung.choi <chayoung.dev@gmail.com>
2019-07-08 22:45:50 +0900

    사용자 입력 history 출력

--- java/src/study/AAD_ConsoleEx/ConsoleEx3.java
+++ java/src/study/AAD_ConsoleEx/ConsoleEx3.java
@@ -1,91 +1,174 @@
 package study.AAD_ConsoleEx;
 
 import java.util.Scanner;
+/*
+[문제3] 사용자가 입력한 명령라인의 내용을 저장하는 save(String input)메서드와 사용자가 입력한 
+	명령라인의 이력을 보여주는 history()메서드를 완성하세요.
+[실행결과]
+>>hello
+hello
+>>hello2
+hello2
+>>hello3
+hello3
+>>hello4
+hello4
+>>history
+1.hello
+2.hello2
+3.hello3
+4.hello4
+5.history
+>>history
+1.hello2
+2.hello3
+3.hello4
+4.history
+5.history
+>>q
 
-/*	190707.
-	[문제2] 사용자가 입력한 명령라인을 공백(' ')을 구분자로 해서 잘라서 배열에 저장한 다음에 배열의 내용을 출력하는 예제. 코드를 완성하세요.
-	[실행결과]
+[출처] [Java1000제] 콘솔 만들기 3 - 도스창 따라하기(history) (남궁성의 코드초보스터디(자바 java, c언어, javascript, python) |작성자 남궁성
+*/
+
+
+import java.util.*;
+
+class ConsoleEx3 {
 	
-	>>java ConsoleEx2.java aaa     bbb      ccc
-	java
-	ConsoleEx2.java
-	aaa
-	bbb
-	ccc
-	>>q
- 
-[출처] [Java1000제] 콘솔 만들기 2 - 도스창 따라하기 (남궁성의 코드초보스터디(자바 java, c언어, javascript, python) |작성자 남궁성
- */
-class ConsoleEx3 { 
-    static String[] argArr;       // 입력한 매개변수를 담기위한 문자열배열 
+	static String[] argArr; // 입력한 매개변수를 담기위한 문자열배열
+	static LinkedList q = new LinkedList(); // 사용자가 입력한 내용을 저장할 큐(Queue)
+	static final int MAX_SIZE = 5; // q(큐)에 저장될 수 있는 값의 개수
 
-    public static void main(String[] args) { 
-    	while(true) { 
-			String prompt = ">>"; 
-			System.out.print(prompt); 
-			
-			// 화면으로부터 라인단위로 입력받는다. 
-		    Scanner s = new Scanner(System.in); 
-		    String input = s.nextLine();
-			
-		    /*
-			    다음의 코드를 완성하세요.
-			    1. 입력받은 값에서 앞뒤 공백을 제거한다. (String클래스의 trim()사용)
-			    2. 입력받은 명령라인의 내용을 공백을 구분자로 해서 나눠서 argArr에 담는다.
-				    String클래스의 split(String regex)를 사용 - 공백이 하나 이상인 경우에도 하나의 공백으로 간주해야함
-				    실행결과를 주의깊게 확인하세요.
-		    */
-		    input.trim();
-		    argArr = input.split(" +");
-		    
-			if (input.equalsIgnoreCase("Q")) { // q 또는 Q를 입력하면 실행종료한다. 
-			      System.exit(0); 
-			} else { 
-			      for(int i=0; i < argArr.length;i++) { 
-			            System.out.println(argArr[i]); 
-			      } 
-			      /* 위의 코드를 향상된 for문으로 변경하면 다음과 같다.
-					  for(String arg : argArr) { 
-					        System.out.println(arg);                                                       
-					  } 
-			      */ 
-			} 
-    	} // while(true) 
-    } // main 
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in); // 한번만 생성해서 재사용하면 되므로 반복문 밖으로 이동
+
+		while (true) {
+			String prompt = ">>";
+			System.out.print(prompt);
+
+			// 화면으로부터 라인단위로 입력받는다.
+			String input = s.nextLine();
+
+			save(input);
+
+			input = input.trim(); // 입력받은 값에서 불필요한 앞뒤 공백을 제거한다.
+			argArr = input.split(" +");
+
+			String command = argArr[0].trim();
+
+			if ("".equals(command))
+				continue;
+
+			command = command.toLowerCase(); // 명령어를 소문자로 바꾼다.
+
+			if (command.equals("q")) { // q 또는 Q를 입력하면 실행종료한다.
+				System.exit(0);
+			} else if (command.equals("history")) { // equalsIgnoreCase대신 equals를 사용.
+				history();
+			} else {
+				for (int i = 0; i < argArr.length; i++) {
+					System.out.println(argArr[i]);
+				}
+			}
+		} // while(true)
+	} // main
+
+	public static void save(String input) {
+		if (input == null || "".equals(input))
+			return;
+
+		/*
+		 * 
+		 * 다음의 코드를 완성하세요.
+		 * 
+		 * 1. queue에 저장한다. 2. queue의 최대크기(MAX_SIZE)를 넣으면 제일 오래된 저장값을 삭제한다.
+		 * 
+		 */
+		q.add(input);
+		if ( q.size() > MAX_SIZE ) {
+			q.removeFirst();
+		}
+	}
+
+	// 사용자가 입력한 최근 명령을 보여주는 메서드
+	public static void history() {
+		int i = 0;
+		/*
+		 * 
+		 * 다음의 코드를 완성하세요. 1. LinkedList에 저장된 내용(최근 MAX_SIZE개의 명령어)을 보여준다.
+		 * 
+		 */
+		for ( int j=0; j<q.size(); j++ ) {
+			System.out.println((j + 1) + "." + q.get(j));
+		}
+	}
 } // class
 /*
 [원문]
-class ConsoleEx2 { 
-      static String[] argArr;       // 입력한 매개변수를 담기위한 문자열배열 
+class ConsoleEx3 { 
+      static String[] argArr;                         // 입력한 매개변수를 담기위한 문자열배열 
+      static LinkedList q = new LinkedList(); // 사용자가 입력한 내용을 저장할 큐(Queue) 
+      static final int MAX_SIZE = 5;              // q(큐)에 저장될 수 있는 값의 개수
+
+ 
 
       public static void main(String[] args) { 
+            Scanner s = new Scanner(System.in); // 한번만 생성해서 재사용하면 되므로 반복문 밖으로 이동
+
             while(true) { 
                   String prompt = ">>"; 
                   System.out.print(prompt); 
                   
                   // 화면으로부터 라인단위로 입력받는다. 
-                  Scanner s = new Scanner(System.in); 
-                  String input = s.nextLine();
-//
-//                     다음의 코드를 완성하세요.
-//
-//                     1. 입력받은 값에서 앞뒤 공백을 제거한다. (String클래스의 trim()사용)
-//                     2. 입력받은 명령라인의 내용을 공백을 구분자로 해서 나눠서 argArr에 담는다.
-//
-//                         String클래스의 split(String regex)를 사용 - 공백이 하나 이상인 경우에도 하나의 공백으로 간주해야함
-//
-//                         실행결과를 주의깊게 확인하세요.
+                  String input = s.nextLine(); 
 
-                  if(input.equalsIgnoreCase("Q")) { // q 또는 Q를 입력하면 실행종료한다. 
+                  save(input); 
+
+                  input = input.trim();        // 입력받은 값에서 불필요한 앞뒤 공백을 제거한다. 
+                  argArr = input.split(" +"); 
+
+
+                  String command = argArr[0].trim(); 
+
+                  if("".equals(command)) continue; 
+
+                  command = command.toLowerCase(); // 명령어를 소문자로 바꾼다. 
+
+                  if(command.equals("q")) { // q 또는 Q를 입력하면 실행종료한다. 
                         System.exit(0); 
+                  } else if(command.equals("history")) {  // equalsIgnoreCase대신 equals를 사용.
+                        history(); 
                   } else { 
                         for(int i=0; i < argArr.length;i++) { 
                               System.out.println(argArr[i]); 
                         } 
-//                        for(String arg : argArr) { 
-//                              System.out.println(arg);                                                       
-//                        } 
-                  }} // while(true)
-} // main
+                  } 
+            } // while(true) 
+      } // main
+
+
+
+      public static void save(String input) { 
+            if(input==null || "".equals(input)) return;
+
+
+//
+//                다음의 코드를 완성하세요.
+//
+//                1. queue에 저장한다. 
+//                2. queue의 최대크기(MAX_SIZE)를 넣으면 제일 오래된 저장값을 삭제한다. 
+//
+      } 
+
+      // 사용자가 입력한 최근 명령을 보여주는 메서드
+      public static void history() { 
+            int i=0; 
+
+//
+//                다음의 코드를 완성하세요.
+//                1. LinkedList에 저장된 내용(최근 MAX_SIZE개의 명령어)을 보여준다. 
+//
+      } 
 } // class
+
 */
